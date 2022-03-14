1abb8f49f41543cd49b0edaa7830d0aae0d69ed5
mcsepro <aremsony2k4@yahoo.com>
2022-03-14 12:08:53 +0100

    task 2 fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-int main (void) {
+int main(void)
+{
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
