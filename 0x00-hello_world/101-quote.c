56393701cc2cd410207a614e3d6d4e03a12431e4
mcsepro <aremsony2k4@yahoo.com>
2022-03-14 08:50:12 -0700

    task 101 fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Return: return 1 (Error)
  */
+
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	printf(
+	stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	);
 	return (1);
 }
