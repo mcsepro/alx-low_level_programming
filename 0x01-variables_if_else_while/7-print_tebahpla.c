777e5d5a4cf3c02191c21c174081c22924f5de18
mcsepro <aremsony2k4@yahoo.com>
2022-03-14 08:12:22 -0700

    task 7 betty pass

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,17 +8,21 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
-    char i;
- 
-    putchar("Capital (upper) case characters:\n");
-    for(i='A'; i<='Z'; i++)
-        putchar("%c ",i);
- 
-    printf("\n\nLower case characters:\n");
-    for(i='a'; i<='z'; i++)
-        putchar("%c ",i);
-     
-    return 0;
+	char i;
+
+	putchar ("Capital (upper) case characters:\n");
+
+	for (i = 'A'; i <= 'Z'; i++)
+
+		putchar ("%c", i);
+
+	printf("\n\nLower case characters:\n");
+
+	for (i = 'a'; i <= 'z'; i++)
+
+		putchar("%c", i);
+
+	return (0);
 }
