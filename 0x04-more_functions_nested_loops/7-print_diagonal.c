2ee24ca2f342815fcb87faf2a9e3b3390115d4ae
mcsepro <aremsony2k4@yahoo.com>
2022-03-17 14:20:35 +0100

    task 7 and 11 updated

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,32 @@
+/*
+ * File: 7-print_diagonal.c
+ * Auth: Martins Arem
+ */
+
 #include "main.h"
 
 /**
- * print_diagonal - check the code for ALX School students.
- * @n: Number of spaces wanted
- * Return: Always 0.
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @n: The number of \ characters to be printed.
  */
 void print_diagonal(int n)
 {
-  int i, j;
+	int len, space;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (len = 0; len < n; len++)
 		{
-			for (j = 0; j < i; j++)
+			for (space = 0; space < len; space++)
 				_putchar(' ');
-
 			_putchar('\\');
+
+			if (len == n - 1)
+				continue;
+
 			_putchar('\n');
 		}
-
 	}
+
+	_putchar('\n');
 }
