856d02566058402119e2aa556bbacdb6fe3d6767
mcsepro <aremsony2k4@yahoo.com>
2022-04-02 13:05:15 -0700

    task updated

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include "main.h"
 
 int main(void)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
     return (0);
-=======
-#include "holberton.h"
-=======
-#include "main.h"
->>>>>>> d3fd93d68cd415076d4a0536636eceff8ad24515
 
-int main(void)
-{
-  _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-  return (0);
->>>>>>> 3bb6a280694ce33215d1411360611bdf7a6bf13b
+
 }
